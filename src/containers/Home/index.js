a60b47a9898a75d8aeb583c1383917bfe41a1ce6
jonathan <jonathansedrez@gmail.com>
2019-09-12 17:22:26 -1200

    add image handler

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 import SEO from '../../components/SEO';
 import Navigation from '../../components/Navigation';
+import Image from '../../components/Image';
 
 const Home = () => (
   <SEO description="site muito bom">
     <Navigation />
     <h1>Home</h1>
+    <Image />
   </SEO>
 );
-
 export default Home;
