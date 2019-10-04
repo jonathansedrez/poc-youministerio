3c0fa2e43413ddcb824e5ac326df087aa4804f93
jonathan.sedrez <jonathan.sedrez@ilegra.com>
2019-10-04 17:11:12 -0300

    add meta tag image

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -3,19 +3,25 @@ import React from 'react';
 import SEO from '../../components/SEO';
 import Navigation from '../../components/Navigation';
 import Image from '../../components/Image';
+import metaImage from '../../images/meta-image.jpg';
 
 const meta = [
   {
     name: `description`,
-    content: 'aushiuhsduiahsuidhsuih',
+    content:
+      'Pois foi Deus quem nos fez o que somos agora; em nossa união com Cristo Jesus, ele nos criou para que fizéssemos as boas obras que ele já havia preparado para nós. Efésios 2:10',
   },
   {
     property: `og:title`,
-    content: 'title show',
+    content: 'You Ministerio | Seu lugar é aqui',
   },
   {
     property: `og:description`,
-    content: 'descrição og',
+    content: 'You Ministerio | Seu lugar é aqui',
+  },
+  {
+    property: `og:image`,
+    content: metaImage,
   },
   {
     property: `og:type`,
