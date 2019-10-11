f0bd5582f7844e57eaa26ac64f2a314332f25aca
jonathan.sedrez <jonathan.sedrez@ilegra.com>
2019-10-11 11:32:49 -0300

    change maps component

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import mapStyle from './mapStyle.json';
+import './styles.css';
 
 const Marker = ({ name }) => {
   return (
-    <div
+    <img
+      src="http://v3.danielmall.com/articles/svg-workflow-for-designers/svg.svg"
       className="marker"
       style={{
         cursor: 'pointer',
         position: 'absolute',
         top: '50%',
         left: '50%',
-        width: '18px',
+        width: '42px',
         height: '18px',
-        backgroundColor: 'red',
-        border: '2px solid #fff',
-        borderRadius: '100%',
         transform: 'translate(-50%, -50%)',
       }}
       title={name}
@@ -32,7 +31,11 @@ function Map({ options }) {
         bootstrapURLKeys={{ key: GOOGLE_API_KEY }}
         defaultCenter={options.center}
         defaultZoom={options.zoom}
-        options={{ styles: mapStyle }}
+        options={{
+          styles: mapStyle,
+          fullscreenControl: false,
+          zoomControl: false,
+        }}
       >
         <Marker
           lat={options.center.lat}
