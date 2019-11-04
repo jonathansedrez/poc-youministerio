528ae4a37a4a3e388a556891bcae6a3a1a44ec09
jonathan.sedrez <jonathan.sedrez@ilegra.com>
2019-11-04 20:18:06 -0300

    change manifest infos

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
@@ -19,13 +14,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `you-ministerio`,
+        short_name: `You Ministerio`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#e3dcc8`,
+        theme_color: `#e3dcc8`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
     {
@@ -35,8 +30,5 @@ module.exports = {
         pageTransitionDelay: 0,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 };
