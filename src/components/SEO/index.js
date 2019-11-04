2df328f2d367cf6c3098a2179a6759d5b5501c39
jonathan.sedrez <jonathan.sedrez@ilegra.com>
2019-11-04 16:44:03 -0300

    change meta tags layout

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -2,24 +2,29 @@ import React, { Fragment } from 'react';
 import { Helmet } from 'react-helmet';
 import GlobalStyle from '../../styles/globalStyles';
 import PropTypes from 'prop-types';
+import Image from '../../images/meta-image.jpg';
 
 function SEO({ meta, title, children }) {
-  const defaultMetaTags = [
-    {
-      property: 'og:type',
-      content: 'website',
-    },
-    {
-      property: 'robots',
-      content: 'index, follow',
-    },
-  ];
+  // const defaultMetaTags = [
+  //   {
+  //     property: 'og:type',
+  //     content: 'website',
+  //   },
+  //   {
+  //     property: 'robots',
+  //     content: 'index, follow',
+  //   },
+  // ];
+  console.log(meta);
 
   return (
     <Fragment>
-      <Helmet meta={meta ? [...defaultMetaTags, ...meta] : defaultMetaTags}>
+      <Helmet>
         <title>{title ? `${title} | You Ministério` : 'You Ministério'}</title>
         <html lang="pt-br" />
+        <meta name="og:image" content={Image} />
+        <meta name="og:type" content="website" />
+        <meta name="robots" content="index,follow" />
       </Helmet>
       <GlobalStyle />
       <main>{children}</main>
